0f480a708c4cd2143218eb38ce715a0c9b59fa95
klowitnarr <nikaneznat@gmail.com>
2025-05-21 16:52:42 +0300

    view для лк + фиксы, есть все еще баг

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,4 +4,4 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/javascript .js//= link_tree ../builds
